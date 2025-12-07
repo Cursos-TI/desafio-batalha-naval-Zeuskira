27d671b52374f3e9710f5030e0c0ddcc498d49ff
Zeuskira <52209081+Zeuskira@users.noreply.github.com>
2025-12-07 01:25:22 +0000

    Nivel Mestre

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,12 +4,7 @@ int main() {
     int tabuleiro[10][10]= {0}; // Inicializa o tabuleiro com zeros
     int i, j; // Variáveis de controle para os loops e indicar linha/coluna
     
-    printf("   "); // Espaço necessário para alinhar os números das colunas
-    for (j = 0; j < 10; j++) { // Loop para imprimir os números das colunas
-        printf("%d ", 1 + j); // Imprime os números de 1 a 10, sem o 1 + j, iria imprimir de 0 a 9
-    }
-    printf("\n");
-
+    // Navios
     int navio1_linha[] = {2, 2, 2}; // Define as posições do navio horizontal
     int navio1_coluna[] = {2, 3, 4};
 
@@ -36,18 +31,56 @@ int main() {
     for (i = 0; i < 3; i++) {
         tabuleiro[navio4_linha[i]][navio4_coluna[i]] = 3;
     }
+    // Habilidades
 
-    for (i = 0; i < 10; i++) { 
-        printf("%c: ", 'A' + i); // Imprime a letra correspondente à linha (A–J), convertendo o índice numérico em caractere ASCII
+    // Cone
+    for(i = 0; i < 10; i++){ 
         for (j = 0; j < 10; j++) {
-            printf("%d ", tabuleiro[i][j]); // Imprime a matriz
-            
-            
+            if (i == 0 && j == 2) {
+                tabuleiro[i][j] = 5;
+            } else if (i == 1 && j >= 1 && j <= 3){
+                tabuleiro[i][j] = 5;
+            } else if (i == 2 && j >= 0 && j <= 4){
+                tabuleiro[i][j] = 5;
         }
+    }
+        }
+    // Cruz   
+    
+    for(i = 0; i < 10; i++){ 
+        for (j = 0; j < 10; j++) {
+            if (i >= 5 && i <= 7 && j == 7 ) {
+                tabuleiro[i][j] = 5;
+            } else if (i == 6 && j >= 5 && j <= 9){
+                tabuleiro[i][j] = 5;
+            }
+        }
+    }
+    // Octaedro
+    for(i = 0; i < 10; i++){ 
+        for (j = 0; j < 10; j++) {
+            if (i >= 7 && i <= 9 && j == 2) {
+                tabuleiro[i][j] = 5;
+            } else if (i == 8 && j >= 1 && j <= 3){
+                tabuleiro[i][j] = 5;
+            }
+        }
+    }    
+    printf("   "); // Espaço necessário para alinhar os números das colunas
+    for (j = 0; j < 10; j++) { // Loop para imprimir os números das colunas
+        printf("%d ", j); // Imprime os números de 0 a 9 para as colunas
+    }
+    printf("\n");
+    
+    for (i = 0; i < 10; i++) { 
+        printf("%d: ", i); // Imprime de 0 a 9 para as linhas
+        for (j = 0; j < 10; j++) { 
+            printf("%d ", tabuleiro[i][j]); // Imprime a matriz do tabuleiro
+        } 
         printf("\n");
         
     }
-    
+   
 
     return 0;
 }
